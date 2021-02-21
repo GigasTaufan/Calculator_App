019876fff5526eb7259345e658baff0e70899912
Gigas Taufan <gigastaufan@gmail.com>
2021-02-21 16:08:23 +0700

    Add matematic function

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'button.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class InputPage extends StatefulWidget {
   InputPage({Key key}) : super(key: key);
@@ -34,8 +35,39 @@ class _InputPageState extends State<InputPage> {
     '.',
     '=',
   ];
+
   @override
   Widget build(BuildContext context) {
+    // Operator button
+    bool isOperator(String x) {
+      if (x == '%' ||
+          x == '/' ||
+          x == 'x' ||
+          x == '-' ||
+          x == '+' ||
+          x == '=') {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // equal button (show answer)
+    void equalPressed() {
+      String finalQuestion = question;
+      // change x into *
+      finalQuestion = finalQuestion.replaceAll('x', '*');
+      // parse the question
+      Parser p = Parser();
+      Expression exp = p.parse(finalQuestion);
+      // bind the variables
+      ContextModel cm = ContextModel();
+      // evaluate expression
+      double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+      answer = eval.toString();
+    }
+
     return Scaffold(
       backgroundColor: Colors.deepOrange[100],
       body: Column(
@@ -116,6 +148,24 @@ class _InputPageState extends State<InputPage> {
                         color: Colors.red,
                       );
                     }
+
+                    // Equal Button =
+                    else if (index == buttons.length - 1) {
+                      return CalculatorButton(
+                        onPressed: () {
+                          setState(() {
+                            equalPressed();
+                          });
+                        },
+                        buttonText: buttons[index],
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                        color: isOperator(buttons[index])
+                            ? Colors.deepOrange[400]
+                            : Colors.deepOrange[50],
+                      );
+                    }
                     // Operator and Number
                     else {
                       return CalculatorButton(
@@ -139,13 +189,5 @@ class _InputPageState extends State<InputPage> {
         ],
       ),
     );
-  }
-
-  bool isOperator(String x) {
-    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
-      return true;
-    } else {
-      return false;
-    }
   }
 }
